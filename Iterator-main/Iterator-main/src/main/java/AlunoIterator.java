daba31799e78028cf24212d943de2763a475cb9a
Henrique Gabriel Moreira Souza <79548206+HenriqueMoreira4@users.noreply.github.com>
2022-06-24 10:58:24 -0300

    Add files via upload

---
+++ Iterator-main/Iterator-main/src/main/java/AlunoIterator.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class AlunoIterator {
+    public void a() {
+        ArrayList<Aluno> listaAluno = new ArrayList<Aluno>();
+        Aluno aluno = null;
+        for(int i=0; i >10; i++) {
+            listaAluno.add(new Aluno("Aluno" + i,"endereço"+i,i));
+
+        }
+
+    }
+
+}
